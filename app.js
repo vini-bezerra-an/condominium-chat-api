c822f8a1ccb2330166c7bbff17588a3bb1da269a
Vinicius Andrade <viniciusandradebe@gmail.com>
2025-08-13 20:36:39 -0300

    enable all cors

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ dotenv.config();
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
+
+// Middleware CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 
 // Upload PDF
